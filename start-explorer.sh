efca6a07cf7f03e34c4855c1e3f83ace86f17c85
DubProdigy <36584821+DubProdigy@users.noreply.github.com>
2020-05-03 21:57:47 -0500

    Update start-explorer.sh
    Added Correct Details

--- start-explorer.sh
+++ start-explorer.sh
@@ -1,6 +1,6 @@
 NETWORK="$1"
 if [ -z "$NETWORK" ]; then
-    NETWORK="testnet"
+    NETWORK="mainnet"
 fi
 HOST="0.0.0.0" PORT="4200" yarn build:"$NETWORK"
-EXPLORER_HOST="0.0.0.0" EXPLORER_PORT="4200" pm2 start /home/headsupracing/headsup-explorer/express-server.js --name explorer
+EXPLORER_HOST="0.0.0.0" EXPLORER_PORT="4200" pm2 start /home/headsupracing/headsup-explorer/express-server.js --name hur-explorer
